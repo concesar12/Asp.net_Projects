62cf818a63700052bf7398306bbd046ddc0eaea0
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-03-27 21:06:18 +1300

    Content Result

--- ControllersExample/ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -3,12 +3,29 @@
 namespace ControllersExample.Controllers // NameOfPrject.Folder
 {
 
-    public class HomeController // It is ncessary to suffix the wordcontroller for the class to be taken as a controller // Class must be public to be instantiated by .netcore
+    public class HomeController : Controller // It is ncessary to suffix the wordcontroller for the class to be taken as a controller 
     {
-        [Route("Hello")] //It is required to define an attribute to specify the route // This is called attribute routing //
-        public string method1() // Method to be accessed
+        [Route("Home")] //It is required to define an attribute to specify the route // This is called attribute routing //
+        [Route("/")] // Both work
+        public ContentResult Index() // Method to be accessed // The name index relates to the main page // ContentResult will have the type to be responded 
         {
-            return "Hello from method1"; 
+            //Instead
+            //return Content("Hello from Index", "text/plain");
+            //return new ContentResult() { Content = "Hello from Index", ContentType = "text/plain" }; // It is necessary to provide a valid content type
+            //Try to return some html
+            return Content("<h1>Hello, </h1>  <h2>This is Cesar</h2>", "text/html");
+        }
+
+        [Route("About")] 
+        public string About() 
+        {
+            return "Hello from About";
+        }
+
+        [Route("Contact-us/{mobile:regex(^\\d{{10}}$)}")] // This is to handle phones only get 10 digits double corchete and slash, because it need to be taken as the regular expression
+        public string Contact() 
+        {
+            return "Hello from Contact";
         }
     }
 }
