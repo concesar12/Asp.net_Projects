dcd420509decda42d759b270342af6cee8df1c9e
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-03-27 20:19:37 +1300

    Creating controllers

---
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers // NameOfPrject.Folder
+{
+
+    public class HomeController // It is ncessary to suffix the wordcontroller for the class to be taken as a controller // Class must be public to be instantiated by .netcore
+    {
+        [Route("Hello")] //It is required to define an attribute to specify the route // This is called attribute routing //
+        public string method1() // Method to be accessed
+        {
+            return "Hello from method1"; 
+        }
+    }
+}
---
+++ ControllersExample/ControllersExample/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers(); // This method AddControllers will get all the classes with the suffix controller to add them
+//builder.Services.AddTransient<HomeController>(); // This is used for smaller projects
+// Above the builder.build is a good place to create any service method
+var app = builder.Build();
+/*THIS IS THE LONG WAY TO DO IT*/
+//app.UseRouting();
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers(); // This will map all the controllers found in the project
+//});
+/*INSTEAD THE MAP CONTROLLER METHOS DOES EVERYTHING DONE ABOVE*/
+app.MapControllers();
+app.Run();
