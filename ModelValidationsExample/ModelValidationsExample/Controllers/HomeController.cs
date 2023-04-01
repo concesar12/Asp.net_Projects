0c2a0f36fc01b4ccabc23cfcb27a02d7ce2d89f5
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-01 20:52:17 +1300

    Model State

--- ModelValidationsExample/ModelValidationsExample/Controllers/HomeController.cs
+++ ModelValidationsExample/ModelValidationsExample/Controllers/HomeController.cs
@@ -8,6 +8,22 @@ namespace ModelValidationsExample.Controllers
         [Route("register")]
         public IActionResult Index(Person person)
         {
+            if (!ModelState.IsValid) // This propperty will check the status of the attributes in the model
+            {
+                /*This will be done using LINQ to make it easier*/
+                //List<string> errorsList = new List<string>();
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach (var error in value.Errors) // This will get all the errors of all propperties
+                //    {
+                //        errorsList.Add(error.ErrorMessage);
+                //    }
+                //}
+                //string errors = string.Join("\n", errorsList);
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+                //List<string> errorsList = ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage).ToList(); // Old way to do it
+                return BadRequest(errors);
+            }
             return Content($"{person}");
         }
     }
