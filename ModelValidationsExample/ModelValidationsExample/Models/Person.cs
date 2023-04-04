119888bc7f2634ce4b6c21255120ec4781ee47fd
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-04 20:09:16 +1200

    Validatable Object

--- ModelValidationsExample/ModelValidationsExample/Models/Person.cs
+++ ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -4,12 +4,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidationsExample.Models
 {
-    public class Person
+    public class Person : IValidatableObject // this only has a method validate then we have to implement the interface
     {
         [Required(ErrorMessage = "{0} can't be empty or null")] // {0} means it will take as an argument the name of the attribute
         [Display(Name = "Person Name")] // This is used to display the name as wanted
         [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")] // this is used to specify the length 1= 40, 2 = 3 from the values
-        [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} should contain only alphabets space and a dot")] // This is to accept names with only letters
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets space and a dot")] // This is to accept names with only letters
         public string? PersonName { get; set; }
         [EmailAddress(ErrorMessage = "{0} should be a normal email address example@example.com")]
         [Required(ErrorMessage = "{0} is mandatory")]
@@ -38,10 +38,21 @@ namespace ModelValidationsExample.Models
         [DateRangeValidator("FromDate", ErrorMessage = "'From Date' should be older than or equal to 'To date'")]
         public DateTime? ToDate { get; set; }
 
+        public int? Age { get; set; }
 
         public override string ToString()
         {
             return $"Person object - Person name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, Confirm Password: {ConfirmPassword}, Price: {Price}";
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) // This was generated automatically because of the interface
+        {
+           if (DateOfBirth.HasValue == false && Age.HasValue == false)
+            {
+                //The yield keyword will allow me to return multiple values they are converted to IEnumerable
+               yield return new ValidationResult("Either of Date of Birth or Age must be supplied", new[] { nameof (Age) }); // nameof is a function that will allow refactoring of names
+                //In order for this to be execute all the other values need to be clear, mistakeless
+            }
         }
     }
 }
