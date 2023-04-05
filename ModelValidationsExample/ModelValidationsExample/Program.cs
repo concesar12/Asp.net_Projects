39816d03415532da2e7b147b4963226cf8e04b51
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-05 20:37:55 +1200

    Input formatters

--- ModelValidationsExample/ModelValidationsExample/Program.cs
+++ ModelValidationsExample/ModelValidationsExample/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddXmlSerializerFormatters(); // This is for input formatter, this will allow the controller read xml or json format
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
+
+
 app.Run();
