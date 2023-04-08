73e3fc00a0365823fcac1f1ab02187b227ef5747
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-08 18:50:49 +1200

    view data 1

--- ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
 
 namespace ViewsExample.Controllers
 {
@@ -8,9 +9,19 @@ namespace ViewsExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            ViewData["appTitle"] = "Example page";
+            List<Person> people = new List<Person>()
+            {
+                new Person() { Name = "Cesar", DateOfBirth = DateTime.Parse("1993-04-05"), PersonGender = Gender.Male },
+                new Person() { Name = "Marcela", DateOfBirth = DateTime.Parse("1995-06-12"), PersonGender = Gender.Female },
+                new Person() { Name = "Laisa", DateOfBirth = DateTime.Parse("2000-11-11"), PersonGender = Gender.Other },
+
+            };
+            ViewData["people"] = people;
             return View(); //Views/Home/Index.cshtml
             //return View("abc"); // abc.cshtml
             //return new ViewResult() { ViewName = "abc" }; this is the long way to very used in the real projects
+
         }
     }
 }
