843147d6296bc2b7a5a836cee26a3701669115f1
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-13 19:13:38 +1200

    View Component 1

---
+++ ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+        Task<IViewComponentResult> InvokeAsync() // This method is mandatory for every view component
+        {
+            return View(); //Invoked a partial view Views/Shared/Components/Grid/Default.cshtml tis is the prefered route
+        }
+    }
+}
