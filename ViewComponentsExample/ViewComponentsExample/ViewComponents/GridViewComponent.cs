b2b4c02a29e623877dac481aa4da9d0acf77378a
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-13 19:52:31 +1200

    View Component 2

--- ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
+++ ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -4,7 +4,7 @@ namespace ViewComponentsExample.ViewComponents
 {
     public class GridViewComponent : ViewComponent
     {
-        Task<IViewComponentResult> InvokeAsync() // This method is mandatory for every view component
+        public async Task<IViewComponentResult> InvokeAsync() // This method is mandatory for every view component
         {
             return View(); //Invoked a partial view Views/Shared/Components/Grid/Default.cshtml tis is the prefered route
         }
