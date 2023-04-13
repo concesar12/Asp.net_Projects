577c6be1c0de20020672ba084cd1280f1daa3fe1
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-13 22:16:04 +1200

    View Component with view data

--- ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
+++ ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
 
 namespace ViewComponentsExample.ViewComponents
 {
@@ -6,6 +7,19 @@ namespace ViewComponentsExample.ViewComponents
     {
         public async Task<IViewComponentResult> InvokeAsync() // This method is mandatory for every view component
         {
+            PersonGridModel model = new PersonGridModel()
+            {
+                GridTitle = "PersonsList",
+                Persons = new List<Person>()
+                {
+                    new Person() { PersonName = "John", JobTitle = "Manager"},
+                    new Person() { PersonName = "Cesar", JobTitle = "CEO"},
+                    new Person() { PersonName = "Lis", JobTitle = "Receptionist"},
+
+                }
+
+            };
+            ViewData["Grid"] = model;
             return View(); //Invoked a partial view Views/Shared/Components/Grid/Default.cshtml tis is the prefered route
         }
     }
