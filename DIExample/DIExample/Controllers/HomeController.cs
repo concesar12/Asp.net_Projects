d45f1247a90feefaf5bd6d0594aa99e97811052e
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-16 20:38:30 +1200

    services 2

---
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+
+        //constructor
+        public HomeController()
+        {
+            //Create object of CitiesService class
+            _citiesService = new CitiesService(); // This is bad practice, instead use Dependency injection
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
