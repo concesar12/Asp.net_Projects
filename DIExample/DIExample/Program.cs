1e8dafc53d09b39c5ceac5091ad8a92165ccf9f2
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-18 22:42:54 +1200

    addTranscient() and others shortcuts

--- DIExample/DIExample/Program.cs
+++ DIExample/DIExample/Program.cs
@@ -3,11 +3,15 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.Add(new ServiceDescriptor(
-    typeof(ICitiesServices), //-> first argument is the interface to be provided
-    typeof(CitiesService),   //-> Second is the one that should be created everytime the first parameter is called
-    ServiceLifetime.Scoped //-> Service lifetime
-));
+//builder.Services.Add(new ServiceDescriptor(
+//    typeof(ICitiesServices), //-> first argument is the interface to be provided
+//    typeof(CitiesService),   //-> Second is the one that should be created everytime the first parameter is called
+//    ServiceLifetime.Scoped //-> Service lifetime
+//));
+//This is the fast and short way to make it 
+//builder.Services.AddTransient<ICitiesServices, CitiesService>();
+builder.Services.AddScoped<ICitiesServices, CitiesService>();
+//builder.Services.AddSingleton<ICitiesServices, CitiesService>();
 
 var app = builder.Build();
 
