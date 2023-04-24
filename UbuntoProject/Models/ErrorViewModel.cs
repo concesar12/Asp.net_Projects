4a5a146d526649493f8009c33bbc6e7b7e4e0d69
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-24 19:36:08 +1200

    Ubuntu trial

---
+++ UbuntoProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UbuntoProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
