d4f2c1318cd8c0f7a5e836a9799d305f5d46e696
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-25 19:36:19 +1200

    Environment in controller

--- EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
+++ EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
@@ -4,10 +4,19 @@ namespace EnvironmentsExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        //contrusctor 
+        public HomeController(IWebHostEnvironment webHostEnvironment) // injection of the service to get the environment variable
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
-
+            //_webHostEnvironment.IsDevelopment(); //this one comes from the constructor which was injected the service  IWebHostEnvironment
+            ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
             return View();
         }
     }
