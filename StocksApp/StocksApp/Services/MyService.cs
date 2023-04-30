a79f277cf5b8066b7ff1604bd35fd280c18a149d
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-04-30 21:35:35 +1200

    HTTP Client 1

---
+++ StocksApp/StocksApp/Services/MyService.cs
@@ -0,0 +1,26 @@
+﻿namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task method()
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("url"),
+                    Method = HttpMethod.Get
+                };
+
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            }
+        }
+    }
+}
