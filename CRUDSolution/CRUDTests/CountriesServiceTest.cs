f5d859b426c65422e6eeae62dbd542fecd07f897
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-05-04 21:58:55 +1200

    Add Country - xUnit 2

---
+++ CRUDSolution/CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using ServiceContracts.DTO;
+using ServiceContracts;
+
+namespace CRUDTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesServiceTest()
+        {
+            //_countriesService = new 
+        }
+    }
+}
---
+++ CRUDSolution/ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO classthat is used as return type for most of CountriesService methods
+    /// </summary>
+    public class CountryResponse
+    {
+        public Guid CountryId { get; set; }
+        public string? CountryName { get; set; }
+    }
+
+    public static class CountryExtensions
+    {
+        public static CountryResponse ToCountryResponse (this Country country) //This will be received in the entities class
+        {
+            return new CountryResponse() { CountryId = country.CountryID, CountryName = country.CountryName };
+        }
+    }
+}
