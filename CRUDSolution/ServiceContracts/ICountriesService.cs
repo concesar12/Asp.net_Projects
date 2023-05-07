20c6392140802587b467392bae5651fcba0440f6
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-05-07 21:05:13 +1200

    Get Country by Country ID Xunit Test

--- CRUDSolution/ServiceContracts/ICountriesService.cs
+++ CRUDSolution/ServiceContracts/ICountriesService.cs
@@ -19,5 +19,12 @@ namespace ServiceContracts
         /// </summary>
         /// <returns>All countries from the list as List of CountryResponse</CountryResponse></returns>
         List<CountryResponse> GetAllCountries();
+
+        /// <summary>
+        /// Returns a country object based on the given country ID
+        /// </summary>
+        /// <param name="countryID">Country ID (guid) to search</param>
+        /// <returns>Matching country as CountryResponse object</returns>
+        CountryResponse? GetCountryByCountryID(Guid? countryID);
     }
 }
