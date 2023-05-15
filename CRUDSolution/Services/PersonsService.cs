6213d8bf5818579e0b15bf8f8b87520032b472b7
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-05-15 21:54:25 +1200

    Get Sorted Persons - xUnit Test

--- CRUDSolution/Services/PersonsService.cs
+++ CRUDSolution/Services/PersonsService.cs
@@ -4,6 +4,7 @@ using ServiceContracts.DTO;
 using ServiceContracts;
 using System.ComponentModel.DataAnnotations;
 using Services.Helpers;
+using ServiceContracts.Enums;
 
 namespace Services
 {
@@ -118,6 +119,11 @@ namespace Services
                 default: matchingPersons = allPersons; break;
             }
             return matchingPersons;
+        }
+
+        public List<PersonResponse> GetSortedPersons(List<PersonResponse> allPersons, string sortBy, SortOrderOptions sortOrder)
+        {
+            throw new NotImplementedException();
         }
     }
 }
