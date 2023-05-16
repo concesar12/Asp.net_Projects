5b2f4960ca3f6826f77561ac6d8529637ed581d1
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-05-16 22:29:37 +1200

    Delete Person unit test creation

--- CRUDSolution/Services/PersonsService.cs
+++ CRUDSolution/Services/PersonsService.cs
@@ -192,5 +192,10 @@ namespace Services
 
             return matchingPerson.ToPersonResponse();
         }
+
+        public bool DeletePerson(Guid? PersonID)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
