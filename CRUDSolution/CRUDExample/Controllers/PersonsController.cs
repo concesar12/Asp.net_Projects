a1b158753c5623bca0758044ca03ecf86b6a8a1f
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-05-20 21:13:40 +1200

    04. Search in List View

--- CRUDSolution/CRUDExample/Controllers/PersonsController.cs
+++ CRUDSolution/CRUDExample/Controllers/PersonsController.cs
@@ -18,9 +18,20 @@ namespace CRUDExample.Controllers
 
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString)
         {
+            ViewBag.SearchFields = new Dictionary<string, string>()
+              {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                { nameof(PersonResponse.Email), "Email" },
+                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                { nameof(PersonResponse.Gender), "Gender" },
+                { nameof(PersonResponse.CountryID), "Country" },
+                { nameof(PersonResponse.Address), "Address" }
+              };
             List<PersonResponse> persons = _personsService.GetAllPersons();
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
 
             return View(persons); //Views/Persons/Index.cshtml
         }
