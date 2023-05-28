69518d8700d9590e2bfa0fff073eaf5bd492ab50
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-05-28 22:53:35 +1200

    15. Generate PDF Files

--- CRUDSolution/CRUDExample/Controllers/PersonsController.cs
+++ CRUDSolution/CRUDExample/Controllers/PersonsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Rotativa.AspNetCore;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
@@ -142,7 +143,7 @@ namespace CRUDExample.Controllers
 
         [HttpPost]
         [Route("[action]/{personID}")]
-        public  async Task<IActionResult> Delete(PersonUpdateRequest personUpdateResult)
+        public async Task<IActionResult> Delete(PersonUpdateRequest personUpdateResult)
         {
             PersonResponse? personResponse = await _personsService.GetPersonByPersonID(personUpdateResult.PersonID);
             if (personResponse == null)
@@ -150,6 +151,20 @@ namespace CRUDExample.Controllers
 
             await _personsService.DeletePerson(personUpdateResult.PersonID);
             return RedirectToAction("Index");
+        }
+
+        [Route("PersonsPDF")]
+        public async Task<IActionResult> PersonsPDF()
+        {
+            //Get list of persons
+            List<PersonResponse> persons = await _personsService.GetAllPersons();
+
+            //Return view as pdf
+            return new ViewAsPdf("PersonsPDF", persons, ViewData)
+            {
+                PageMargins = new Rotativa.AspNetCore.Options.Margins() { Top = 20, Right = 20, Bottom = 20, Left = 20 },
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Landscape
+            };
         }
 
     }
