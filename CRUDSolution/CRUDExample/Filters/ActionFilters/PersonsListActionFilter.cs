bf907b4cc06cb8de18df71fb1ea0d2984ec273f6
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-06-08 22:10:10 +1200

    02. Parameter Validation in Action Filter

--- CRUDSolution/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
+++ CRUDSolution/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts.DTO;
 
 namespace CRUDExample.Filters.ActionFilters
 {
@@ -24,6 +25,32 @@ namespace CRUDExample.Filters.ActionFilters
         {
             //To do: add before logic here
             _logger.LogInformation("PersonsListActionFilter.OnActionExecuting method");
+
+            if (context.ActionArguments.ContainsKey("searchBy"))
+            {
+                string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+
+                //validate the searchBy parameter value
+                if (!string.IsNullOrEmpty(searchBy))
+                {
+                    var searchByOptions = new List<string>() {
+      nameof(PersonResponse.PersonName),
+      nameof(PersonResponse.Email),
+      nameof(PersonResponse.DateOfBirth),
+      nameof(PersonResponse.Gender),
+      nameof(PersonResponse.CountryID),
+      nameof(PersonResponse.Address)
+     };
+
+                    //reset the searchBy paramer value
+                    if (searchByOptions.Any(temp => temp == searchBy) == false)
+                    {
+                        _logger.LogInformation("searchBy actual value {searchBy}", searchBy);
+                        context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+                        _logger.LogInformation("searchBy updated value {searchBy}", searchBy);
+                    }
+                }
+            }
         }
     }
 }
